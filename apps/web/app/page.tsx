959c1613df81a9c7949accf1c651d0b19aa987e2
shelliev <shelliev@github.com>
2025-10-07 15:48:23 -0400

    homepage

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>Welcome to My Dashboard</h1>
+      <h1>Welcome to My Dashboard!!</h1>
       <p>Choose your role:</p>
       <ul>
         <li><Link href="/student">Student</Link></li>
