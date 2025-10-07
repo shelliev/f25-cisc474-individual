393629ee7cefa435c35d378b727dc47f2657f1e6
shelliev <shelliev@github.com>
2025-10-06 21:41:27 -0400

    fixing CORS

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    credentials:true,
+  })
   const port = process.env.PORT || 3000;
   const host = process.env.HOST || undefined;
   await app.listen(port, host);
