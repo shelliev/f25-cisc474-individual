a4935065d065cec3c12887b97803a80cd05610d1
shelliev <shelliev@github.com>
2025-10-07 15:03:42 -0400

    push before working on front end

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,7 +6,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'https://f25-cisc474-individual-web-rust.vercel.app',
+    //can add other methods to expand LMS
     credentials:true,
   })
   const port = process.env.PORT || 3000;
